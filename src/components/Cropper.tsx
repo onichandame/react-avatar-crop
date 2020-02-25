87f01adb1be9eb1572874c38c65f95f5eedb7482
Zhang Xiao <zhangxiao@neuboron.com>
2020-02-25 17:02:17 +0800

    staged

--- src/components/Cropper.tsx
+++ src/components/Cropper.tsx
@@ -1,13 +1,14 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useCallback } from "react";
 import EasyCrop from 'react-easy-crop'
+import { Area } from 'react-easy-crop/types'
 import styled from 'styled-components'
 
 import Slider from './Slider'
 
 interface Props {
   image: string;
-  result?: string;
   aspect?: number;
+  onChange(arg: string): any;
 }
 
 const StyledSlider = styled.div`
@@ -17,16 +18,43 @@ const StyledSlider = styled.div`
   z-index: 1000;
 `
 
-const Cropper: FC<Props> = ({ image, aspect = 1 }) => {
+const Cropper: FC<Props> = ({ image, aspect = 1, onChange }) => {
   const [crop, setCrop] = useState<EasyCrop['props']['crop']>({x:0, y:0})
   const [zoom, setZoom] = useState<EasyCrop['props']['zoom']>(1)
   const [rotation, setRotation] = useState<EasyCrop['props']['rotation']>(0)
+
+  const handleCrop = useCallback(async ({} = {}, croppedAreaPixels: Area) => {
+    onChange(await getCroppedImage(image, croppedAreaPixels, rotation))
+  }, [image, rotation, crop, zoom])
+
+  const getCroppedImage = async (src: string, area: Area, rot: typeof rotation) => {
+    let img = await createImage(src)
+
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    const canvasSide = Math.sqrt(img.width**2 + img.height**2)
+    canvas.width = canvasSide
+    canvas.height = canvasSide
+
+    let result = ''
+    return result
+  }
+
+  const createImage = (url: string): Promise<HTMLImageElement> => new Promise((r, j) => {
+    const img = new Image()
+    img.addEventListener('load', () => r(img))
+    img.addEventListener('error', e => j(e))
+    img.setAttribute('crossOrigin', 'anonymous')
+    img.src = url
+  })
+
   return (
     <div style={{position: 'relative', width: '400px', height: '267px'}}>
       <EasyCrop
         image={image}
         crop={crop}
         onCropChange={c => setCrop(c)}
+        onCropComplete={handleCrop}
         zoom={zoom}
         onZoomChange={z => setZoom(z)}
         rotation={rotation}
